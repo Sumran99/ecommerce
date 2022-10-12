2aa397ad217d48d0fbc6e4cc7d1f85db04b88c44
sumran99 <sumran150ak@gmail.com>
2022-10-12 17:30:13 +0500

    adding the card for a product and map the products from api to grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Product from "../src/components/Product";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = React.useState([]);
+  const getAllProducts = () => {
+    axios
+      .get(`http://localhost:3000/`)
+      .then((res) => {
+        setProducts(res.data.product);
+      })
+      .catch((error) => {
+        console.log(`error`);
+      });
+  };
+  React.useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, sm: 4, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {products.map((product, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Product
+                name={product.name}
+                category={product.category}
+                price={product.price}
+                desc={product.description}
+                stock={product.stock}
+                image_url={product.image_url}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
   );
 }
 
